68595e434500e15f81c078882bcb91e331b897ca
Developers at MTN Haus <dev@mtnhausdigital.com>
2024-09-17 15:46:43 +0200

    FGS-134 Replace default home page content

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex">
+        <title>FGS App</title>
+
+        <style>
+            main {
+                text-align: center;
+                padding: 20px;
+                font-family: Arial, sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <main>
+            <p>FGS App v1.0</p>
+        </main>
+    </body>
+</html>
